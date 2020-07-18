a5662f015fcda344741785b2ee50a131d8d5b686
Jia <jiavon@ualberta.ca>
2020-07-18 00:46:34 -0600

    added consts and let

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 //JavaScript for Contact Form are based on (Grieve, 2020) method. References are available in README.md.
-//camelCase and PascalCase were used in this JavaScript according to (Wieruch, 2019)
+//camelCase and PascalCase were used in this JavaScript according to (Wieruch, 2019).
 
 //---IMPORTANT!!----
 //Part of the rubric of this assignment was to only use 'let' and 'const'. No 'var' is allowed.
 //No hardcoding allowed; use 'constants' and 'values' instead.
-
 
 //<input type="email" name="email" maxlength="50">
 //const uses camelCase
@@ -16,5 +15,11 @@ const emailSubject = document.getElementById("subject");
 //<textarea name="body">
 const textArea = document.getElementById("body");
 
+//<button type="submit" name="submit" id="submit">Submit</button>
+const submitButton = document.getElementById("submit");
+
 //Mandatory Rubric: Word filter from Battlestar Galactica: "feldercarb", "frack", "skinjob", "vulgacarb"
-let wordArray =["feldercarb","frack","skinjob","vulgacarb"];
+//I was hoping for Rick and Morty's Sea Cucumber :(
+let wordArray = ["feldercarb","frack","skinjob","vulgacarb"];
+
+//Using Grieve, 2020 Shopping List method
